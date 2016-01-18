8f543ff8bace16f6571dabb1bed3f8f4ce4672b6
adobs <dobs@u.northwestern.edu>
2016-01-18 10:05:25 -0800

    made legal board, need to work on starting board

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,3 @@
-// // main.js
-// var React = require('react');
-// var ReactDOM = require('react-dom');
 $(function() {
 
     function generateBoard(){
@@ -10,125 +7,163 @@ $(function() {
             row.push(null);
         }
         for (var j=0; j < 9; j++){
-            boardMatrix.push(row);
+            boardMatrix.push([row]);
         }
-        populateBoard(boardMatrix);
+        return [[4,7,9,6,5,2,1,3,8],[6,8,5,3,4,1,9,7,2],[3,1,2,8,9,7,6,4,5],[2,5,1,9,7,3,4,8,6],[8,9,3,1,6,4,2,5,7],[7,6,4,5,2,8,3,9,1],[5,2,8,4,3,6,7,1,9],[9,3,6,7,1,5,8,2,4],[1,4,7,2,8,9,5,6,3]]
+        // return populateBoard(boardMatrix);
     }
-
-    function populateBoard(board){
-
-        function checkMove(value, row, col){
-            for (var move=0; move<9; move++){
-                if(board[row][move]===value){
-                    return false;
-                }
-                if (board[move][col]===value){
-                    return false;
-                }
+// 
+    // function populateBoard(board){
+// 
+        // var availableMoves;
+        // var valueIndex;
+        // for (var row=0; row<9; row++){
+            // console.log("row is"+row);
+            // availableMoves = [1,2,3,4,5,6,7,8,9];
+            // for (var col=0; col<9; col++){
+                // valueIndex = Math.floor(Math.random() * availableMoves.length);
+                // if (checkMove(availableMoves[valueIndex], row, col)){
+                    // board[row][col] = availableMoves[valueIndex];
+                    // availableMoves.splice(valueIndex, 1);
+                // }else{
+                    // col--;
+                    // var value = board[row][col];
+                    // board[row][col] = null;
+                    // availableMoves.push(value);
+                    // if (col>0){
+                        // col--;
+                    // }else{
+                        // row--;
+                        // col=9;
+                    // }
+                    // 
+                // }
+// 
+            // }
+        // }
+// 
+        // function checkMove(value, row, col){
+            // for (var move=0; move<9; move++){
+                // check row
+                // if(board[row][move]===value){
+                    // return false;
+                // }
+                // check column
+                // if (board[move][col]===value){
+                    // return false;
+                // }
+            // }
+            // check square
+            // var row_square = Math.floor(row/3);
+            // var col_square = Math.floor(col/3);
+// 
+            // for (var row_count=row_square*3; row_count<=row_square*3+2; row_count++){
+                // for (var col_count=col_square*3; col_count<=col_square*3+2; col_count++){
+                    // if(board[row_count][col_count]===value){
+                        // console.log('square faliure');
+                        // return false;
+                    // }
+                // }
+            // }
+            // return true;
+// 
+        // }
+        // return board;
+    // }
+// 
+    
+    function pickPermanent(){
+        var possibleOptions = []
+        for (var k=0; k<9; k++){
+            for (var j=0; j<9; j++){
+                possibleOptions.push([k, j])
             }
-            return true;
-
         }
-
-        var availableMoves;
-        var valueIndex;
-        for (var row=0; row<9; row++){
-            availableMoves = [1,2,3,4,5,6,7,8,9];
-            for (var col=0; col<9; col++){
-                console.log("board"+board);
-                valueIndex = Math.floor(Math.random()* availableMoves.length);
-                if (checkMove(valueIndex, row, col)){
-                    board[row][col] = availableMoves[valueIndex];
-                    availableMoves.splice(valueIndex, 1);
-                }else{
-                    col--;
-                    if (col>=0){
-                        var value = board[row][col];
-                        board[row][col] = null;
-                        availableMoves.push(value);
-                        col--;
-                    }else{
-                        availableMoves = [1,2,3,4,5,6,7,8,9];
-                        // clear the row
-                        for (var k=0; k<9; k++){
-                            board[row][k] = null;
-                        }
-                        row--;
-                    }
-                }
-
-            }
-        }
-        console.log("BOARD IS"+ board);
-
+        var permanent = _.sample(possibleOptions, 20);
+        console.log('permanent is'+permanent);
+        return permanent;
     }
 
     var Board = React.createClass({
+        getInitialState: function(){ 
+            var layout = generateBoard();
+            console.log('layout is'+layout);
+            var permanentNumbers = pickPermanent()
+            return {boardLayout: layout, numberSelected: null, permanent: permanentNumbers};
+        },
+
+        numberSelected: function(number){
+            this.setState({numberSelected: number});
+            console.log("number selected is"+this.state.numberSelected);
+        },
+
+        isPermanent: function(row, column){
+            var permanentNumbers = this.state.permanent;
+            if ($.inArray([row,column], permanentNumbers)===-1){
+                return false;
+            }
+            return true;
+
+        },
+
         render: function(){
-            return (
+            var num = this.state.numberSelected;
+            var board = this.state.boardLayout;
+            var permanent = pickPermanent();
+            console.log('permie is '+ permanent)
+            return ( 
                 <div id='board'>
-                    <div id='row1'>
-                        <Block9/>
-                        <Block9/>
-                        <Block9/>
-                    </div>
-                    <div id='row2'>
-                        <Block9/>
-                        <Block9/>
-                        <Block9/>
-                    </div>
-                    <div id='row3'>    
-                        <Block9/>
-                        <Block9/>
-                        <Block9/>
-                    </div>
+                    {this.state.boardLayout.map(function(row) {
+                        return (
+                          <div>
+                            {row.map(function(col) {
+                              return <Cell row={board.indexOf(row)} col={row.indexOf(col)} innerValue={col} value={col} numSelected={num}/>;
+                            })}
+                          </div>
+                        );
+                      })}
                     <div>
-                        <Numbers/>
+                        <Numbers numSelected={this.numberSelected}/>
                     </div>
                 </div>
             );
 
-        }
-    });
-
-
-    var Block9 = React.createClass({
-        render: function(){
-            return (
-                <div id='block-9'>
-                    <Cell/>
-                    <Cell/>
-                    <Cell/><br/>
-                    <Cell/>
-                    <Cell/>
-                    <Cell/><br/>
-                    <Cell/>
-                    <Cell/>
-                    <Cell/>
-                </div>
-            );
         }
     });
 
     var Cell = React.createClass({
+        clickedCell: function(evt){
+            if (this.props.numSelected){
+                // $(evt.target).attr("disabled",true);
+                evt.target.innerHTML = this.props.numSelected;
+                // $(evt.target).val(this.props.numberSelected);
+            }
+        },
         render: function(){
-            return <button>1</button>;
+            console.log("row"+this.props.row);
+            console.log("column"+this.props.col);
+            return <button onClick={this.clickedCell}>{this.props.value}</button>
         }
+
+
     });
 
     var Numbers = React.createClass({
+        changeNumber: function(evt){
+            this.props.numSelected(evt.target.innerHTML);
+        },
         render: function(){
             return (
                 <div>
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
-                    <button>7</button>
-                    <button>8</button>
-                    <button>9</button>
+                    <button onClick={this.changeNumber}>1</button>
+                    <button onClick={this.changeNumber}>2</button>
+                    <button onClick={this.changeNumber}>3</button>
+                    <button onClick={this.changeNumber}>4</button>
+                    <button onClick={this.changeNumber}>5</button>
+                    <button onClick={this.changeNumber}>6</button>
+                    <button onClick={this.changeNumber}>7</button>
+                    <button onClick={this.changeNumber}>8</button>
+                    <button onClick={this.changeNumber}>9</button>
                 </div>
             );
 
@@ -137,7 +172,6 @@ $(function() {
 
     ReactDOM.render(
       <Board/>,
-      document.getElementById('example')
+      document.getElementById('sudoku')
     );
-    generateBoard();
 });
