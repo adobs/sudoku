53fd38308aafeb2c2c5b17351b0053acf25d25c3
adobs <dobs@u.northwestern.edu>
2016-01-18 16:24:22 -0800

    later commit

--- static/js/main.js
+++ static/js/main.js
@@ -80,7 +80,7 @@ $(function() {
             }
         }
         var permanent = _.sample(possibleOptions, 20);
-        console.log('permanent is'+permanent);
+        console.log('permanent is length'+permanent.length);
         return permanent;
     }
 
@@ -88,7 +88,7 @@ $(function() {
         getInitialState: function(){ 
             var layout = generateBoard();
             console.log('layout is'+layout);
-            var permanentNumbers = pickPermanent()
+            var permanentNumbers = pickPermanent();
             return {boardLayout: layout, numberSelected: null, permanent: permanentNumbers};
         },
 
@@ -97,52 +97,91 @@ $(function() {
             console.log("number selected is"+this.state.numberSelected);
         },
 
-        isPermanent: function(row, column){
-            var permanentNumbers = this.state.permanent;
-            if ($.inArray([row,column], permanentNumbers)===-1){
-                return false;
-            }
-            return true;
-
-        },
-
         render: function(){
             var num = this.state.numberSelected;
             var board = this.state.boardLayout;
-            var permanent = pickPermanent();
-            console.log('permie is '+ permanent)
-            return ( 
-                <div id='board'>
-                    {this.state.boardLayout.map(function(row) {
+            var permanent = this.state.permanent;
+            var counter = 0;
+            return (
+                <div>
+                    <div id='board'>
+                    {this.state.boardLayout.map(function(val1) {
                         return (
-                          <div>
-                            {row.map(function(col) {
-                              return <Cell row={board.indexOf(row)} col={row.indexOf(col)} innerValue={col} value={col} numSelected={num}/>;
+                          <div id='board-row'>
+                            {val1.map(function(val2) {
+                                counter += 1;
+                                return <Cell row={board.indexOf(val1)} id={counter} key={counter} col={val1.indexOf(val2)} innerValue={val2} permanent={permanent} value={val2} numSelected={num}/>;
                             })}
                           </div>
                         );
                       })}
+                    </div>
                     <div>
                         <Numbers numSelected={this.numberSelected}/>
                     </div>
+                    <Check />
                 </div>
             );
 
         }
     });
+    
+    var Check = React.createClass({
+        checkBoard: function(evt){
+            for (var w=0; w<80; w++){
+                if $("#"+i)}
+            console.log("hereee");
+            console.log(Cell.innerValue)
+            $("div#board-row").each(function(){
+                console.log("here");
+                console.log($(this).text());
+                console.log($(this).children())
+
+            })
+
+        },
+        render: function(){
+            return <button onClick={this.checkBoard}>Check</button>
+        }
+    });
 
     var Cell = React.createClass({
+        getInitialState: function(){
+            var row = this.props.row;
+            var column = this.props.col;
+            var value = this.showValue(row, column, this.props.innerValue);
+            var permanentBoolean;
+            if(value===0){
+                permanentBoolean = false;
+                }else{
+                permanentBoolean = true;
+            }
+            return {permanent: permanentBoolean, value: value}
+
+        },
         clickedCell: function(evt){
-            if (this.props.numSelected){
+            if (this.props.numSelected && this.state.permanent === false){
                 // $(evt.target).attr("disabled",true);
                 evt.target.innerHTML = this.props.numSelected;
                 // $(evt.target).val(this.props.numberSelected);
             }
         },
+
+
+        showValue: function(row, column, value){
+            for (var p=0; p<20; p++){
+                console.log("[rc]"+[row, column])
+                if (row === this.props.permanent[p][0] && column === this.props.permanent[p][1]){
+                    return 0;
+                }
+            }
+            return value;
+
+        },
+
         render: function(){
-            console.log("row"+this.props.row);
-            console.log("column"+this.props.col);
-            return <button onClick={this.clickedCell}>{this.props.value}</button>
+         
+            return <button onClick={this.clickedCell}>{this.state.value}</button>
         }
 
 
