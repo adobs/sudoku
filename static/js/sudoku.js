4eaa6aaacbf75535eb8a6e144133f5c35a127b05
Alexandra Dobkin <Alexandra Dobkin>
2016-01-26 19:01:46 -0800

    made buttons bigger on smaller device

--- static/js/sudoku.js
+++ static/js/sudoku.js
@@ -7,6 +7,16 @@ $(function() {
     $(".intro").height($(window).height());
     $("#about").height($(window).height());
 
+    $(window).resize(function(){     
+
+           if ($('.intro').width() == 767 ){
+
+                $(".btn-board").width(($("#sudoku").width())/10)
+                $(".btn-board").height(($("#sudoku").width())/10)
+
+           }
+
+    });
 
     var Board = React.createClass({
         getInitialState: function(){ 
